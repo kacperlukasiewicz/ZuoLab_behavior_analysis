f02e5f33754960e76a2a6cb564ab2ce1ff148ccd
kacperlukasiewicz <51636561+kacperlukasiewicz@users.noreply.github.com>
2020-08-07 17:53:50 +0200

    Update behavior_analysis_05_EPM.py

--- behavior_analysis_05_EPM.py
+++ behavior_analysis_05_EPM.py
@@ -22,6 +22,7 @@ Pipeline:
 
 #%%
 import os
+import glob
 import numpy as np
 import math
 from matplotlib import pyplot as plt
@@ -31,47 +32,38 @@ from PIL import Image
 #%%
 path_name = 'C:\\Users\\Kacper\\Documents\\ASD_computational_ethology\\EPM_analysis'
 
-file_name = 'LM174MR_ctlDLC_resnet50_PlusMazeJul22shuffle1_10000.csv'
+file_names = glob.glob(path_name + '\\' + '*12000.csv')
 
-file = path_name + '\\' + file_name
-print(file)
-
-#%%
-# video calibration
-fps = 30
-# arena measured from upper left to lower right corner
-arena_length_cm = 110
-arena_width_cm = 110
-arena_length_pxl = 976
-arena_width_pxl = 968
-calibration = ((arena_length_pxl / arena_length_cm) +  (arena_width_pxl / arena_width_cm)) / 2 #1cm = n pxl
+#file_name = 'LM174MR_ctlDLC_resnet50_PlusMazeJul22shuffle1_12000.csv'
+#file = path_name + '\\' + file_name
 
 #%%
-# bcg frame extraction
-#video_input = file[0:-4]+'_labeled.mp4'
-video_input = file[0:-4]+'_labeled.mp4'
-bcg_frame_name = file[0:-4]+'_frame01.png'
-os.system('ffmpeg -ss 00:00:01 -i {0} -vframes 1 -q:v 2 {1}'.format(video_input, bcg_frame_name))
-
-#%%
-ULcorner = (926, 514)
-URcorner = (1000, 517)
-LRcorner = (1000, 597)
-LLcorner = (925, 597)
-left_x_border = 925
-right_x_border = 1000
-upper_y_border = 515
-lower_y_border = 597
-
-#%%
-def fn_parameters_log (file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border):
+def fn_parameters_log (file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border, limit):
     log_table = []
-    log = ('file', 'fps', 'calibration', 'left_x_border', 'right_x_border', 'upper_y_border', 'lower_y_border')
+    log = ('file', 'fps', 'calibration', 'left_x_border', 'right_x_border', 'upper_y_border', 'lower_y_border', 'limit')
     log_table.append(log)
-    log = (file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border)
+    log = (file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border, limit)
     log_table.append(log)
     return log_table
 
+def fn_sum (file, limit, param):
+    lines = open(file).readlines()
+    sum_file = 0
+    for line in lines[0:limit]:
+        record = float(line.split(',')[param].strip())
+        sum_file = sum_file + record
+    sum_file_list = [sum_file]
+    return sum_file_list
+
+def fn_average (file, limit, param):
+    lines = open(file).readlines()
+    sum_file = 0
+    for line in lines[0:limit]:
+        record = float(line.split(',')[param].strip())
+        sum_file = sum_file + record
+    average_file = sum_file / limit
+    average_file_list = [average_file]
+    return average_file_list
 
 def fn_body_part (body_part, xy, line):
     if body_part == 'nose':
@@ -222,35 +214,211 @@ def fn_save_bodypart_velocity_plot (file, bodypart, bcg_filename):
                 #c=cm.jet(fn_norm_bpart_velocity(fn_bpart_velocity (file, 'nose'))),
                 marker = '.')
     plt.savefig(file[0:-4]+ '_' + bodypart + '_velocity_plot.png')
+    
+
+def fn_in_arm (file, left_x_border, right_x_border, upper_y_border, lower_y_border):
+    in_arm_table = []
+    lines = open(file).readlines()
+    prev_armLeft = 0
+    prev_armRight = 0
+    prev_armUpper = 0
+    prev_armLower = 0
+    armLeft_entry = 0
+    armRight_entry = 0
+    armUpper_entry = 0
+    armLower_entry = 0
+    for line in lines[3:]:
+        armLeft = 0
+        armRight = 0
+        armUpper = 0
+        armLower = 0
+        try: 
+            neckx = fn_body_part ('neck', 'x', line)
+            necky = fn_body_part ('neck', 'y', line)
+            tailx= fn_body_part ('tail', 'x', line)
+            taily = fn_body_part ('tail', 'y', line)
+            if neckx < left_x_border and tailx < left_x_border:
+                armLeft = 1
+                if prev_armLeft == 0:
+                    armLeft_entry = 1
+                else:
+                    armLeft_entry = 0
+                
+                prev_armLeft = armLeft
+                prev_armRight = 0
+                prev_armUpper = 0
+                prev_armLower = 0
+                    
+            if necky < upper_y_border and taily < upper_y_border:
+                armUpper = 1
+                if prev_armUpper == 0:
+                    armUpper_entry = 1
+                else:
+                    armUpper_entry = 0
+                    
+                prev_armUpper = armUpper
+                prev_armRight = 0
+                prev_armLeft = 0
+                prev_armLower = 0
+                    
+            if neckx > right_x_border and tailx > right_x_border:
+                armRight = 1
+                if prev_armRight == 0:
+                    armRight_entry = 1
+                else:
+                    armRight_entry = 0
+                    
+                prev_armRight = armRight
+                prev_armLeft = 0
+                prev_armUpper = 0
+                prev_armLower = 0
+                
+            if necky > lower_y_border and taily > lower_y_border:
+                armLower = 1
+                if prev_armLower == 0:
+                    armLower_entry = 1
+                else:
+                    armLower_entry = 0
+                    
+                prev_armLower = armLower
+                prev_armRight = 0
+                prev_armUpper = 0
+                prev_armLeft = 0
+                
+            in_arm_record = (armLeft, armRight, armUpper, armLower, armLeft_entry, armRight_entry, armUpper_entry, armLower_entry)
+            in_arm_table.append(in_arm_record)
+            
+        except ValueError:
+            in_arm_table.append('NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN', 'NaN')
+    return in_arm_table
+    
 
 print("done")
 
+
 #%%
-np.savetxt(file[0:-4]+'_parameters_log.csv', 
-           fn_parameters_log(file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border), delimiter=',', fmt="%s")
 
+for file in file_names:
+    print(file)
 
-np.savetxt(file[0:-4]+'_body_distance.csv', 
-           fn_bpart_distance_moved (file, 'body'), delimiter=',', fmt="%s")
-
-np.savetxt(file[0:-4]+'_nose_distance.csv', 
-           fn_bpart_distance_moved (file, 'nose'), delimiter=',', fmt="%s")
-
-np.savetxt(file[0:-4]+'_bparts_Euclidean_distance.csv', 
-           fn_all_bpart_distance (file), 
-           delimiter=',', fmt="%f")
-
-
-np.savetxt(file[0:-4]+'_nose_velocity.csv', 
-           fn_bpart_velocity(file, 'nose'), delimiter=',', fmt="%s")
-
-np.savetxt(file[0:-4]+'_body_velocity.csv', 
-           fn_bpart_velocity(file, 'body'), delimiter=',', fmt="%s")
-
-#np.savetxt(file[0:-4]+'_body_velocity_sqrt.csv', 
-#           fn_sqrt_bpart_velocity(fn_bpart_velocity (file, 'body')), delimiter=',', fmt="%s")
-
-
-fn_save_bodypart_velocity_plot (file, 'body', bcg_frame_name)
-
-fn_save_bodypart_velocity_plot (file, 'nose', bcg_frame_name)
+    #%%
+    # video calibration
+    
+    limit = 7317
+    try:
+        parameters = open(file[0:-4]+'_parameters_log.csv').readlines()
+        fps = float(parameters[1].split(',')[1].strip())
+        calibration = float(parameters[1].split(',')[2].strip())
+        #limit = float(parameters[1].split(',')[7].strip())
+    except IOError: 
+        fps = 24.39
+        # arena measured from upper left to lower right corner
+        arena_length_cm = 65 # 30+30+5
+        arena_width_cm = 65 # 30+30+5
+        arena_length_pxl = 976
+        arena_width_pxl = 968
+        calibration = ((arena_length_pxl / arena_length_cm) +  (arena_width_pxl / arena_width_cm)) / 2 #1cm = n pxl
+        #limit = 7317
+    
+    #%%
+    # bcg frame extraction
+    #video_input = file[0:-4]+'_labeled.mp4'
+    
+    #video_input = file[0:-4]+'_labeled.mp4'
+    #bcg_frame_name = file[0:-4]+'_frame01.png'
+    bcg_frame_name = file[0:-44]+'_frame01.png'
+    #os.system('ffmpeg -ss 00:00:01 -i {0} -vframes 1 -q:v 2 {1}'.format(video_input, bcg_frame_name))
+    
+    #%%
+    try:
+        parameters = open(file[0:-4]+'_parameters_log.csv').readlines()
+        left_x_border = float(parameters[1].split(',')[3].strip())
+        right_x_border = float(parameters[1].split(',')[4].strip())
+        upper_y_border = float(parameters[1].split(',')[5].strip())
+        lower_y_border = float(parameters[1].split(',')[6].strip())
+    except IOError: 
+        ULcorner = (926, 514)
+        URcorner = (1000, 517)
+        LRcorner = (1000, 597)
+        LLcorner = (925, 597)
+        left_x_border = 925
+        right_x_border = 1000
+        upper_y_border = 515
+        lower_y_border = 597
+    
+    
+    
+    #%%
+    np.savetxt(file[0:-4]+'_parameters_log.csv', 
+               fn_parameters_log(file, fps, calibration, left_x_border, right_x_border, upper_y_border, lower_y_border, limit), 
+               delimiter=',', fmt="%s")
+    
+    
+    np.savetxt(file[0:-4]+'_body_distance.csv', 
+               fn_bpart_distance_moved (file, 'body'), delimiter=',', fmt="%s")
+    
+    np.savetxt(file[0:-4]+'_nose_distance.csv', 
+               fn_bpart_distance_moved (file, 'nose'), delimiter=',', fmt="%s")
+    
+    np.savetxt(file[0:-4]+'_bparts_Euclidean_distance.csv', 
+               fn_all_bpart_distance (file), 
+               delimiter=',', fmt="%f")
+    
+    
+    np.savetxt(file[0:-4]+'_nose_velocity.csv', 
+               fn_bpart_velocity(file, 'nose'), delimiter=',', fmt="%s")
+    
+    np.savetxt(file[0:-4]+'_body_velocity.csv', 
+               fn_bpart_velocity(file, 'body'), delimiter=',', fmt="%s")
+    
+    #np.savetxt(file[0:-4]+'_body_velocity_sqrt.csv', 
+    #           fn_sqrt_bpart_velocity(fn_bpart_velocity (file, 'body')), delimiter=',', fmt="%s")
+    
+    
+    fn_save_bodypart_velocity_plot (file, 'body', bcg_frame_name)
+    
+    fn_save_bodypart_velocity_plot (file, 'nose', bcg_frame_name)
+    
+    np.savetxt(file[0:-4]+'_in_arm.csv', 
+               fn_in_arm(file, left_x_border, right_x_border, upper_y_border, lower_y_border), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_in_arm_left_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 0), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_in_arm_right_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 1), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_in_arm_upper_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 2), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_in_arm_lower_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 3), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_entries_left_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 4), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_entries_right_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 5), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_entries_upper_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 6), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_entries_lower_sum.csv', 
+               fn_sum(file[0:-4]+'_in_arm.csv', limit, 7), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_body_distance_sum.csv', 
+               fn_sum(file[0:-4]+'_body_distance.csv', limit, 0), 
+               delimiter=',', fmt="%f")
+    
+    np.savetxt(file[0:-4]+'_body_velocity_average.csv', 
+               fn_average(file[0:-4]+'_body_velocity.csv', limit, 0), 
+               delimiter=',', fmt="%f")
